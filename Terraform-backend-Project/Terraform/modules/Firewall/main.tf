3a85de00bdddad42b11cbe1a3b0cd600fd469d7f
Priyanka-Bisht <76993764+Priyanka-Bisht@users.noreply.github.com>
2023-12-15 12:30:56 +0530

    Update main.tf

--- Terraform-backend-Project/Terraform/modules/Firewall/main.tf
+++ Terraform-backend-Project/Terraform/modules/Firewall/main.tf
@@ -1,13 +1,27 @@
 /******************************************
 	Firewall configuration
  *****************************************/
+resource "google_compute_firewall" "lb_health_check_firewall" {
+  name    = "allow-health-check"
+  network = var.network_name
+  priority = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080"]  
+  }
+
+  source_ranges = ["0.0.0.0/0"]  # Allowing traffic from any source (update as needed)
+}
+
+
 resource "google_compute_firewall" "firewall" {
   name    = var.firewall_name
   network = var.network_name
 
+  priority = 1001
   allow {
     protocol = "tcp"
-    ports    = ["80", "8080", "22"]
+    ports    = ["22"]
   }
   source_ranges = ["0.0.0.0/0"]
 }
@@ -24,14 +38,3 @@ resource "google_compute_firewall" "firewall" {
 #   source_ranges = ["0.0.0.0/0"]  # Allowing traffic from any source (update as needed)
 # }
 
-resource "google_compute_firewall" "lb_health_check_firewall" {
-  name    = "allow-health-check"
-  network = var.network_name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["443"]  # Adjust based on your load balancer health check configuration
-  }
-
-  source_ranges = ["0.0.0.0/0"]  # Allowing traffic from any source (update as needed)
-}
