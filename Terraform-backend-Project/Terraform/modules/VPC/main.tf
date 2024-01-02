149d44da5150bd5c23e18e32e3314d9e325e411c
Priyanka-Bisht <76993764+Priyanka-Bisht@users.noreply.github.com>
2024-01-02 12:40:44 +0530

    Update main.tf

--- Terraform-backend-Project/Terraform/modules/VPC/main.tf
+++ Terraform-backend-Project/Terraform/modules/VPC/main.tf
@@ -30,20 +30,3 @@ resource "google_compute_subnetwork" "subnetwork" {
     ip_cidr_range = var.secondary_ip_cidr_range
   }
 }
-
-
-
-/*****************************************
-                  Router
-***********************************************/
-resource "google_compute_router" "router" {
-  name    = "my-router"
-  network = google_compute_network.network.name
-}
-
-resource "google_compute_router_nat" "cloud_nat" {
-  name               = "cloud-nat"
-  router             = google_compute_router.router.name
-  nat_ip_allocate_option = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
