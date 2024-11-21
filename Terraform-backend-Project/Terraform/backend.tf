41bebc88cb9c160292232e09620fc78848d8819c
Priyanka-Bisht <76993764+Priyanka-Bisht@users.noreply.github.com>
2024-11-21 12:08:58 +0530

    Update backend.tf

--- Terraform-backend-Project/Terraform/backend.tf
+++ Terraform-backend-Project/Terraform/backend.tf
@@ -1,7 +1,15 @@
 terraform {
   backend "gcs" {
-    bucket = "bucketforappengine121"
-    prefix = "bucketforappengine121/state"
+    bucket         = "bucketforappengine121"  # Replace with your GCS bucket name
+    prefix         = "terraform/state"
+    project        = var.project_id
+    #credentials = file("keyfile.pem")       # Optional: Set a prefix for your state files
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">=4.0.0"
+    }
 
   }
 }
